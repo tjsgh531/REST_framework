383fced222fc082c32e18f0c1bf3433701822ae7
Sun Ho <tjsgh5312@daum.net>
2022-08-01 13:04:55 +0900

    compltet tutorial 2

--- tutorial_ad/snippets/views.py
+++ tutorial_ad/snippets/views.py
@@ -1,3 +1,4 @@
+"""
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,13 +8,14 @@ from snippets.serializers import SnippetSerializer
 
 @csrf_exempt #CSRF token을 가지고 있지 않는 사용자도 이 뷰에 POST요청 할 수 있도록 하는 역할
 def snippet_list(request):
-    """
-    코드 snippet의 목록을 보여주거나, 새로운 snippet 생성
-    """
+    
+    # 코드 snippet의 목록을 보여주거나, 새로운 snippet 생성
+    
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many = True)
         return JsonResponse(serializer.data, safe=False)
+
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = SnippetSerializer(data = data)
@@ -24,9 +26,9 @@ def snippet_list(request):
 
 @csrf_exempt
 def snippet_detail(request, pk):
-    """
-    snippet을 검색, 업데이트, 삭제
-    """
+    
+    # snippet을 검색, 업데이트, 삭제
+    
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
@@ -50,3 +52,52 @@ def snippet_detail(request, pk):
     elif request.method == 'DELETE':
         snippet.delete()
         return HttpResponse(status=204)
+"""
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
+    """
+    코드 snippet의 목록을 보여주거나, 새로운 snippet생성
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUSET)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
+    """
+    snippet을 검색, 업데이트, 삭제
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method =='PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method =='DELETE':
+        serializer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
